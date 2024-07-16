78e16c9aa5ad7ba53cc9933c74ed6067005dede3
EvelinaSarg <126480724+EvelinaSarg@users.noreply.github.com>
2024-07-17 00:28:30 +0100

    Update app_stock.py

--- app_stock.py
+++ app_stock.py
@@ -65,7 +65,7 @@ else:
     # Apply custom styling to the latest data table
     def style_specific_columns(row):
         color = 'lightgreen' if row['percentage_change'] > 0 else 'lightcoral'
-        return ['background-color: {}'.format(color) if col in ['ticker', 'percentage_change'] else
+        return ['background-color: 'lightyellow' if col=='ticker' else
                 'background-color: lightblue' if col == 'open' else
                 'background-color: lightpink' if col == 'high' else
                 'background-color: lightyellow' if col == 'date' else
